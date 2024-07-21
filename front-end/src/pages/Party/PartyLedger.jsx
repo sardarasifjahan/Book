4357fdaec6870f892afc8dd9ff47ef48d5596c46
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-07-21 08:54:32 +0530

    Book Initial

--- front-end/src/pages/Party/PartyLedger.jsx
+++ front-end/src/pages/Party/PartyLedger.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import { Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Grid, MenuItem, Select } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {
+    Button,
+    Grid,
+    MenuItem,
+    Paper,
+    Select,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from '@mui/material';
+import {useSelector} from 'react-redux';
 
+const PartyLedger = ({partyName}) => {
+    const {partyUser} = useSelector((state) => state.partyReducerValue);
+    const [transactions, setTransactions] = useState([]);
 
-const PartyLedger = ({ party  }) => {
-    const transactions = [
-        { date: '2024-05-28', voucher: 'Sales Invoice', srNo: 2, credit: 200, debit: 200, tdsByParty: '-', tdsBySelf: '-', balance: 200 },
-        { date: '2024-05-28', voucher: 'Sales Invoice', srNo: 3, credit: 200, debit: 200, tdsByParty: '-', tdsBySelf: '-', balance: 200 },
-    ];
+    useEffect(() => {
+        console.log("Party Ledger   " + partyName);
+        const details = partyUser.find(item => item.id === partyName);
+        console.log("Party Details values", details);
+        setTransactions(details ? details.transactions : []);
+    }, [partyName, partyUser]);
 
     return (
         <div>
-            <Grid container justifyContent="space-between" alignItems="center" sx={{ mb: 2 }}>
+            <Grid container justifyContent="space-between" alignItems="center" sx={{mb: 2}}>
                 <Select defaultValue="Last 365 Days">
                     <MenuItem value="Last 30 Days">Last 30 Days</MenuItem>
                     <MenuItem value="Last 90 Days">Last 90 Days</MenuItem>
                     <MenuItem value="Last 365 Days">Last 365 Days</MenuItem>
                 </Select>
                 <div>
-                    <Button variant="outlined" sx={{ mr: 1 }}>Download</Button>
+                    <Button variant="outlined" sx={{mr: 1}}>Download</Button>
                     <Button variant="outlined">Print</Button>
                 </div>
             </Grid>
-            <Typography variant="h6" gutterBottom>{party}</Typography>
+            <Typography variant="h6" gutterBottom>{partyName}</Typography>
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
@@ -37,18 +55,26 @@ const PartyLedger = ({ party  }) => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {transactions.map((transaction, index) => (
-                            <TableRow key={index}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.voucher}</TableCell>
-                                <TableCell>{transaction.srNo}</TableCell>
-                                <TableCell>{transaction.credit}</TableCell>
-                                <TableCell>{transaction.debit}</TableCell>
-                                <TableCell>{transaction.tdsByParty}</TableCell>
-                                <TableCell>{transaction.tdsBySelf}</TableCell>
-                                <TableCell>{transaction.balance}</TableCell>
+                        {transactions.length > 0 ? (
+                            transactions.map((transaction, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{transaction.date}</TableCell>
+                                    <TableCell>{transaction.voucher}</TableCell>
+                                    <TableCell>{transaction.srNo}</TableCell>
+                                    <TableCell>{transaction.credit}</TableCell>
+                                    <TableCell>{transaction.debit}</TableCell>
+                                    <TableCell>{transaction.tdsByParty}</TableCell>
+                                    <TableCell>{transaction.tdsBySelf}</TableCell>
+                                    <TableCell>{transaction.balance}</TableCell>
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={8} align="center">
+                                    No transactions available
+                                </TableCell>
                             </TableRow>
-                        ))}
+                        )}
                     </TableBody>
                 </Table>
             </TableContainer>
